79c88f4b45c1020d7bc3db893deeeb5fb9f42077
Verónica Reina <veroreinah@gmail.com>
2018-07-15 13:55:35 +0200

    Authentication

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePicPath: String,
+  profilePicName: String
+});
+UserSchema.set("timestamps", true);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
