44beab8052bdb1805ba8cf8a7ded538aee212046
Verónica Reina <veroreinah@gmail.com>
2018-07-15 18:47:26 +0200

    Authentication views

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,53 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  /* Crop profile image */
+  const resizerWrp = document.getElementById('resizer-wrp');
+  const fileInput = document.getElementById('picture');
+  const signupForm = document.getElementById("signupForm");
 
-  console.log('IronGenerator JS imported successfully!');
+  fileInput.onchange = () => {
+    readFile(fileInput);
+  };
 
+  const croppie = new Croppie(resizerWrp, {
+    viewport: {
+      width: 200,
+      height: 200,
+      type: "circle"
+    },
+    boundary: {
+      width: 220,
+      height: 220
+    },
+    mouseWheelZoom: false
+  });
+
+  const readFile = input => {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+  
+      reader.onload = e => {
+        resizerWrp.classList.remove("d-none");
+        croppie.bind({
+            url: e.target.result
+          })
+          .then(() => {
+            console.log("Bind complete");
+          });
+      };
+  
+      reader.readAsDataURL(input.files[0]);
+    }
+  };
+
+  signupForm.onsubmit = () => {
+    croppie.result({
+        type: "base64"
+      })
+      .then(resp => {
+        document.getElementById("profilePic").value = resp;
+        signupForm.submit();
+      });
+    
+    return false;
+  }
 }, false);
