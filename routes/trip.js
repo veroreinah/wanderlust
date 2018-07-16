4d05f53127ac2744df7f021a0f64c5c5ee1e3c3f
Verónica Reina <veroreinah@gmail.com>
2018-07-16 19:15:42 +0200

    List user's trips

--- routes/trip.js
+++ routes/trip.js
@@ -4,10 +4,16 @@ const express = require("express");
 const router = express.Router();
 const { ensureLoggedIn } = require("connect-ensure-login");
 const Trip = require("../models/Trip");
+const moment = require('moment');
 
 router.get("/", ensureLoggedIn(), (req, res, next) => {
-  Trip.find({})
+  Trip.find({ userId: req.user._id })
     .then(trips => {
+      trips.forEach(t => {
+        t.date_start = t.dateStart ? moment(t.dateStart).format('MM/DD/YYYY') : "";
+        t.date_end = t.dateEnd ? moment(t.dateEnd).format('MM/DD/YYYY') : "";
+      });
+
       res.render("trip/index", {
         trips,
         tripsActive: " active"
