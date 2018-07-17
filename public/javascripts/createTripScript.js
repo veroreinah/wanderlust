a230fc5394eb218c4afd73f1a5747ace1743902f
Verónica Reina <veroreinah@gmail.com>
2018-07-17 13:09:30 +0200

    Daterangepicker and chosen libraries

--- public/javascripts/createTripScript.js
+++ public/javascripts/createTripScript.js
@@ -1,3 +1,24 @@
 $(function() {
-  $("#dates").daterangepicker();
+  // Destination
+  const destinationField = $("#destination");
+  destinationField.chosen();
+
+  $("#multiDestination").on("change", function() {
+    if ($(this).val() === "true") {
+      destinationField.attr("multiple", true);
+    } else {
+      destinationField.attr("multiple", false);
+    }
+    destinationField.chosen("destroy");
+    destinationField.chosen();
+  });
+
+  // Dates
+  const datesField = $("#dates");
+  datesField.daterangepicker({
+    autoUpdateInput: false
+  });
+  datesField.on('apply.daterangepicker', function(ev, picker) {
+    $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+  });
 });
