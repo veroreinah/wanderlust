1a8b19c655b0d25f768f23e484c9419884185f05
Verónica Reina <veroreinah@gmail.com>
2018-07-18 20:36:41 +0200

    Home styles

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index', {
+router.get("/", (req, res, next) => {
+  res.render("index", {
+    images: [
+      { class: "current", img: "/images/bg/bg1.jpg" },
+      { class: "", img: "/images/bg/bg2.jpg" },
+      { class: "", img: "/images/bg/bg3.jpg" },
+      { class: "", img: "/images/bg/bg4.jpg" },
+      { class: "", img: "/images/bg/bg5.jpg" },
+      { class: "", img: "/images/bg/bg6.jpg" }
+    ],
     homeActive: " active"
   });
 });
