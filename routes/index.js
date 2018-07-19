0fd2cc9435163eb7e6963a2019e4c9977995eb25
Verónica Reina <veroreinah@gmail.com>
2018-07-19 17:23:34 +0200

    Get inspired page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
+var _ = require('lodash');
+const axios = require("axios");
+const axiosOptions = {
+  baseURL: process.env.SYGIC_URL,
+  headers: { "x-api-key": process.env.SYGIC_API_KEY }
+};
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -18,6 +24,39 @@ router.get("/", (req, res, next) => {
 
 router.get("/credits", (req, res, next) => {
   res.render("credits");
+});
+
+router.get("/inspiration", (req, res, next) => {
+  axios.get(`/places/list?limit=50&levels=poi`, axiosOptions)
+    .then(result => {
+      const places = result.data.data.places;
+      const shuffledPlaces = _.slice(_.shuffle(places), 0, 10);
+      const apiPromises = [];
+
+      shuffledPlaces.forEach(element => {
+        apiPromises.push(axios.get(`/places/${element.id}`, axiosOptions));
+      });
+
+      return Promise.all(apiPromises);
+    })
+    .then(result => {
+      const resultPlaces = [];
+      result.forEach(e => {
+        resultPlaces.push(e.data.data.place);
+      });
+
+      const places1 = _.slice(resultPlaces, 0, 5);
+      const places2 = _.slice(resultPlaces, 5, 10);
+
+      res.render("inspiration", {
+        places1,
+        places2,
+        inspiActive: " active"
+      });
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
